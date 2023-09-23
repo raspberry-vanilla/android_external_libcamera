84afb38c934d8bc872aadec4e335aa2127367e67
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2023-09-23 18:23:33 +0200

    android: Stub GraphicBufferAllocator for build tests
    If we want to keep building libcamera on traditional Linux systems with:
      -Dandroid=enabled -Dandroid_platform=generic
    
    We should stub GraphicBufferAllocator, which is not available.
    It's only available when building with the VNDK or when building within the
    AOSP tree.
    
    Also remove some deprecated methods and inclusions which are not needed for
    the stub class.
    
    Note: the imported headers from Android generate the -Wextra-semi warning.
          To avoid patching the files, a pragma has been added before inclusion.
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- include/android/frameworks/native/libs/ui/include/ui/GraphicBufferAllocator.h
+++ include/android/frameworks/native/libs/ui/include/ui/GraphicBufferAllocator.h
@@ -29,14 +29,9 @@
 #include <ui/PixelFormat.h>
 
 #include <utils/Errors.h>
-#include <utils/KeyedVector.h>
-#include <utils/Mutex.h>
 #include <utils/Singleton.h>
 
 namespace android {
-
-class GrallocAllocator;
-class GraphicBufferMapper;
 
 class GraphicBufferAllocator : public Singleton<GraphicBufferAllocator>
 {
@@ -51,25 +46,6 @@ public:
     status_t allocate(uint32_t w, uint32_t h, PixelFormat format, uint32_t layerCount,
                       uint64_t usage, buffer_handle_t* handle, uint32_t* stride,
                       std::string requestorName);
-
-    /**
-     * Allocates and does NOT import a gralloc buffer. Buffers cannot be used until they have
-     * been imported. This function is for advanced use cases only.
-     *
-     * The raw native handle must be freed by calling native_handle_close() followed by
-     * native_handle_delete().
-     */
-    status_t allocateRawHandle(uint32_t w, uint32_t h, PixelFormat format, uint32_t layerCount,
-                               uint64_t usage, buffer_handle_t* handle, uint32_t* stride,
-                               std::string requestorName);
-
-    /**
-     * DEPRECATED: GraphicBufferAllocator does not use the graphicBufferId.
-     */
-    status_t allocate(uint32_t w, uint32_t h, PixelFormat format,
-            uint32_t layerCount, uint64_t usage,
-            buffer_handle_t* handle, uint32_t* stride, uint64_t graphicBufferId,
-            std::string requestorName);
 
     status_t free(buffer_handle_t handle);
 
@@ -94,15 +70,9 @@ protected:
                             uint64_t usage, buffer_handle_t* handle, uint32_t* stride,
                             std::string requestorName, bool importBuffer);
 
-    static Mutex sLock;
-    static KeyedVector<buffer_handle_t, alloc_rec_t> sAllocList;
-
     friend class Singleton<GraphicBufferAllocator>;
     GraphicBufferAllocator();
     ~GraphicBufferAllocator();
-
-    GraphicBufferMapper& mMapper;
-    std::unique_ptr<const GrallocAllocator> mAllocator;
 };
 
 // ---------------------------------------------------------------------------
