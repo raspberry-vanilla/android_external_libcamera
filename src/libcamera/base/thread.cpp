cd8614f6289e0924f114be9abacba985146b2061
Konsta <konsta09@gmail.com>
2025-02-14 17:59:17 +0200

    Revert "libcamera: add method to set thread affinity"
    * Doesn't compile on AOSP15.
    
    This reverts commit 4d9db06d669044c0c461a2aed79c85c7fe32a502.

--- src/libcamera/base/thread.cpp
+++ src/libcamera/base/thread.cpp
@@ -9,7 +9,6 @@
 
 #include <atomic>
 #include <list>
-#include <optional>
 #include <pthread.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
@@ -134,8 +133,6 @@ private:
 	int exitCode_;
 
 	MessageQueue messages_;
-
-	std::optional<cpu_set_t> cpuset_;
 };
 
 /**
@@ -292,8 +289,6 @@ void Thread::startThread()
 	if (!name_.empty())
 		pthread_setname_np(pthread_self(), name_.substr(0, 15).c_str());
 
-	setThreadAffinityInternal();
-
 	run();
 }
 
@@ -421,48 +416,6 @@ bool Thread::wait(utils::duration duration)
 		thread_.join();
 
 	return hasFinished;
-}
-
-/**
- * \brief Set the CPU affinity mask of the thread
- * \param[in] cpus The list of CPU indices that the thread is set affinity to
- *
- * The CPU indices should be within [0, std::thread::hardware_concurrency()).
- * If any index is invalid, this function won't modify the thread affinity and
- * will return an error.
- *
- * \return 0 if all indices are valid, -EINVAL otherwise
- */
-int Thread::setThreadAffinity(const Span<const unsigned int> &cpus)
-{
-	const unsigned int numCpus = std::thread::hardware_concurrency();
-
-	MutexLocker locker(data_->mutex_);
-	data_->cpuset_ = cpu_set_t();
-	CPU_ZERO(&data_->cpuset_.value());
-
-	for (const unsigned int &cpu : cpus) {
-		if (cpu >= numCpus) {
-			LOG(Thread, Error) << "Invalid CPU " << cpu << "for thread affinity";
-			return -EINVAL;
-		}
-
-		CPU_SET(cpu, &data_->cpuset_.value());
-	}
-
-	if (data_->running_)
-		setThreadAffinityInternal();
-
-	return 0;
-}
-
-void Thread::setThreadAffinityInternal()
-{
-	if (!data_->cpuset_)
-		return;
-
-	const cpu_set_t &cpuset = data_->cpuset_.value();
-	pthread_setaffinity_np(thread_.native_handle(), sizeof(cpuset), &cpuset);
 }
 
 /**
